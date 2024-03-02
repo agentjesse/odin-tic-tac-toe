5c69f6446bc778e5fa0d2d90c6daf3da22a696cc
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-02 14:18:14 -0500

    gameboardModule updated
    added the state array and function that returns the lines to check

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-//gameboard object: have state of game in an array. public functions to: 1.set 
-//the state array 2. get array of state of all the possible three-in-a-rows
+//gameboard object: have state of board(x's and o's) in an array. public functions to: 1.set 
+//the state array 2. get array of state of all the possible three-in-a-rows lines
 const gameboardModule = (function () {
   const boardArr = new Array(9).fill(null);
-  const logBoard = ()=> console.log(boardArr);
+  [ boardArr[0], boardArr[3], boardArr[6] ]=['X','X','X']; //test using destructuring assignment
+
+  const logBoardArr = ()=> console.log(boardArr); //for debugging
+
+  const getCheckLines = ()=> {
+    return {
+      row1: [boardArr[0],boardArr[1],boardArr[2]],
+      row2: [boardArr[3],boardArr[4],boardArr[5]],
+      row3: [boardArr[6],boardArr[7],boardArr[8]],
+
+      col1: [boardArr[0],boardArr[3],boardArr[6]],
+      col2: [boardArr[1],boardArr[4],boardArr[7]],
+      col3: [boardArr[2],boardArr[5],boardArr[8]],
+      
+      forwardDiag: [boardArr[6],boardArr[4],boardArr[2]],
+      backDiag:    [boardArr[0],boardArr[4],boardArr[8]],
+    }
+  }
 
   //public exposure object, references closure scope
-  return {logBoard};
+  return {logBoardArr,getCheckLines};
 })();
 
 
