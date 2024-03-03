8b19fa08471d94c903fcaa1a0911a62e320e9c2e
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-03 12:20:52 -0500

    Added line check logic
    gameboard will do the checking and return a result obj. also added a
    message box instead of console logs.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 //gameboard object: have state of board(x's and o's) in an array. 
-//public functions: get board state array for analyzing; get 3 in a row lines as
-//strings to check them; update display using state array; mark board state
-//array with index and mark, then update display; clear the board
+//public functions: get board state array for analyzing; update display using state array; mark board state array with index and mark, then update display; clear the board; check board and return a result obj
 const gameboard = ( ()=> {
   //make array of 9 zero-indexed elements, every three represents a row.
   const boardArr = new Array(9).fill(null);
-  // [ boardArr[0], boardArr[3], boardArr[6] ]=['X','X','X']; //test using destructuring assignment
+  // [ boardArr[2], boardArr[5], boardArr[8] ]=['O','O','O']; //test using destructuring assignment
 
   const getBoardArr = ()=> boardArr;
 
@@ -35,7 +33,7 @@ const gameboard = ( ()=> {
     if (!boardArr[cellIndex]){//only mark if cell is empty
       boardArr[cellIndex] = marker;
       updateDisplay();
-      console.log(boardArr);
+      console.log('current boardArr: ',boardArr);//for debugging
     }else {
       console.log('oops, cell occupied');
     }
@@ -46,8 +44,20 @@ const gameboard = ( ()=> {
     updateDisplay();
   }
 
+  const checkForWinner = ()=> {
+    const checkLines = getCheckLines()
+    for (const key in checkLines){
+      if(checkLines[key] === 'XXX'){ //variable key name, need to use square brackets
+        return {winner:'X', line: key};
+      }
+      else if (checkLines[key] === 'OOO'){
+        return {winner:'O', line: key};
+      }
+    }
+  }
+
   //public exposure object, will reference closure scope
-  return {getBoardArr, getCheckLines, updateDisplay, markBoard, clearBoard};
+  return {getBoardArr, updateDisplay, markBoard, clearBoard, checkForWinner};
 })();
 
 //player objects factory function. make player objects from an event handler. commented code is just for closure learning purposes, i.e. private variables example.
@@ -78,6 +88,7 @@ const gameFlow = ( ()=> {
   //local vars
   const xNameInput = document.querySelector('#xName');
   const oNameInput = document.querySelector('#oName');
+  const messageBox = document.querySelector('#messageBox');
   let playerX, playerO, currentPlayer;
   //***ABORT CONTROLLER IS NOT BEING USED*** KEPT JUST FOR LEARNING PURPOSES.**
   //allow listener removal via the single-use AbortController AbortSignal object
@@ -91,7 +102,10 @@ const gameFlow = ( ()=> {
       e.stopPropagation;
       if (e.target.className === 'boardCell'){
         gameboard.markBoard(e.target.dataset.cell, currentPlayer.marker)//mark board, using playerX for testing
-        
+        const result = gameboard.checkForWinner();
+        if (result){ //truthy value means result obj with winner came back
+          messageBox.textContent = `${result.winner === 'X' ? playerX.name: playerO.name} wins! __line key: ${result.line}`;
+        }
       }
     },{signal:controller.signal}); //providing an AbortSignal object allows removal of this listener
   }
@@ -106,11 +120,11 @@ const gameFlow = ( ()=> {
       oNameInput.value = oNameInput.value ? oNameInput.value : 'Player 2';
       playerX = player(xNameInput.value,'X');
       currentPlayer = playerX;
-      console.log(`${currentPlayer.name} goes first!`)
+      messageBox.textContent = `${currentPlayer.name} goes first!`
       playerO = player(oNameInput.value,'O');
       enableGameboard();
     }
-    //clear name fields and board, disable gameboard
+    //clear board to restart game. extra code is to disable gameboard and clear names
     if (e.target.id === 'restart'){
       gameboard.clearBoard();
       // [ xNameInput.value, oNameInput.value ] = ['','']; //fancy assignment example
